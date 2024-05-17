f35a5c4dfb22df0d28768621433b5a4ae350b4a4
Daria <ms.dluk@gmail.com>
2024-05-17 19:18:47 +0300

    add smt

--- blue_veil_site.py
+++ blue_veil_site.py
@@ -10,6 +10,21 @@ app.config['UPLOAD_FOLDER'] = 'uploads'
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
+def parse_classification_report(report):
+    lines = report.split('\n')
+    parsed_report = []
+    for line in lines[2:-3]:
+        row = list(filter(None, line.split(' ')))
+        if len(row) > 5:
+            parsed_report.append({
+                'class': row[0],
+                'precision': row[1],
+                'recall': row[2],
+                'f1-score': row[3],
+                'support': row[4]
+            })
+    return parsed_report
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -23,9 +38,13 @@ def upload_file():
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
             result = fast_pred(file_path)
-            accuracy = first_pred(file_path)  # Get accuracy from model creation
-            print(accuracy, result)
-            return render_template('result.html', result=result, accuracy=accuracy)
+            accuracy_report = first_pred(file_path)  # Get accuracy from model creation
+            
+            # Parse classification report
+            parsed_report = parse_classification_report(accuracy_report)
+            print(parse_classification_report)
+            print(accuracy_report)
+            return render_template('result.html', result=result, parsed_report=parsed_report)
     return render_template('upload.html')
 
 @app.route('/result')
